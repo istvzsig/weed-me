226b5f3664ac973a2d0fc0d60f1438a676f10c21
istvzsig <istvanzsigmond@aol.com>
2026-01-11 20:28:32 +0100

    hh config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-ethers";
 
-export default defineConfig({
-  solidity: {
-    version: "0.8.28",
+const config = {
+  solidity: "0.8.28",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
   },
-});
+};
+
+export default config;
