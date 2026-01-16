df5523d1d5940879c0a72b06d137e9950bc0c9b8
istvzsig <istvanzsigmond@aol.com>
2026-01-16 09:45:07 +0100

    next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
+import path from "path";
+import dotenv from "dotenv"
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
 /** @type {import('next').NextConfig} */
-const nextConfig: import("next").NextConfig = {
-  turbopack: {
-    root: __dirname,
+const nextConfig = {
+  turbopack: { root: __dirname },
+  env: {
+    WEED_TOKEN_ADDRESS: process.env.WEED_TOKEN_ADDRESS,
+    PLANT_NFT_ADDRESS: process.env.PLANT_NFT_ADDRESS,
+    FARM_GAME_ADDRESS: process.env.FARM_GAME_ADDRESS,
   },
 };
 
