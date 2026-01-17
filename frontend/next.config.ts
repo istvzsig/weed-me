da5216ba100578ece42014daf2c9a618998c7ba9
istvzsig <istvanzsigmond@aol.com>
2026-01-17 21:53:30 +0100

    next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,14 @@
 import path from "path";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 /** @type {import('next').NextConfig} */
-const nextConfig: import('next').NextConfig = {
+const nextConfig: import("next").NextConfig = {
   turbopack: { root: __dirname },
   env: {
+    FRONTEND_PORT: process.env.FRONTEND_PORT,
+    BACKEND_PORT: process.env.BACKEND_PORT,
     WEED_TOKEN_ADDRESS: process.env.WEED_TOKEN_ADDRESS,
     PLANT_NFT_ADDRESS: process.env.PLANT_NFT_ADDRESS,
     FARM_GAME_ADDRESS: process.env.FARM_GAME_ADDRESS,
